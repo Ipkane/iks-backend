fba7f4dea65f778a8257ee0d04248fa780266f9c
Igor Kaynov <igor.kaynov@waveaccess.ru>
2016-03-17 21:39:28 +0300

    db connection

--- src/main/java/com/iks/cms/web/config/BasicConfig.java
+++ src/main/java/com/iks/cms/web/config/BasicConfig.java
@@ -4,47 +4,57 @@ package com.iks.cms.web.config;
 ** config data source , entity manager here
 */
 
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.context.annotation.*;
+import org.springframework.jdbc.datasource.*;
+import org.springframework.orm.jpa.*;
+import org.springframework.orm.jpa.vendor.*;
+import org.springframework.transaction.*;
+import org.springframework.transaction.annotation.*;
+
+import java.util.*;
+
+import javax.sql.*;
 
 @Configuration
 //@EnableJpaRepositories( basePackages = { "com.makble.springmvcstart.repository" } )
-//@EnableTransactionManagement
+@EnableTransactionManagement
 @ComponentScan( basePackages = { "com.iks.cms.web.service", "com.iks.cms.web.repository", "com.iks.cms.web.controller" } )
 public class BasicConfig {
-//  @Autowired
-//  private DataSource dataSource;
-//  @Bean
-//  public DataSource dataSource() {
-//    DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//    dataSource.setDriverClassName( "com.mysql.jdbc.Driver" );
-//    dataSource.setUrl( "jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=UTF-8" );
-//    dataSource.setUsername( "root" );
-//    dataSource.setPassword( "rootadm" );
-//    return dataSource;
-//  }
-//  @Bean
-//  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-//    HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-//    vendorAdapter.setDatabase( Database.POSTGRESQL );
-//    vendorAdapter.setGenerateDdl( true );
-//    vendorAdapter.setShowSql( true );
-//    LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-//    factory.setJpaVendorAdapter( vendorAdapter );
-//    factory.setPackagesToScan( getClass().getPackage().getName() );
-//    factory.setDataSource( dataSource() );
-//    factory.setJpaProperties( jpaProperties() );
-//    return factory;
-//  }
-//  private Properties jpaProperties() {
-//    Properties properties = new Properties();
-//    properties.put( "hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect" );  // MySQL5InnoDBDialect   MySQLMyISAMDialect
-//    properties.put( "hibernate.show_sql", "true" );
-//    return properties;
-//  }
-//  @Bean
-//  public PlatformTransactionManager transactionManager() {
-//    JpaTransactionManager txManager = new JpaTransactionManager();
-//    txManager.setEntityManagerFactory( entityManagerFactory().getObject() );
-//    return txManager;
-//  }
+  @Autowired
+  private DataSource dataSource;
+  @Bean
+  public DataSource dataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName( "org.postgresql.Driver" );
+    dataSource.setUrl( "jdbc:postgresql://localhost:5432/postgres" );
+    dataSource.setUsername( "postgres" );
+    dataSource.setPassword( "postgres" );
+    return dataSource;
+  }
+  @Bean
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+    vendorAdapter.setDatabase( Database.POSTGRESQL );
+    vendorAdapter.setGenerateDdl( true );
+    vendorAdapter.setShowSql( true );
+    LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+    factory.setJpaVendorAdapter( vendorAdapter );
+    factory.setPackagesToScan( getClass().getPackage().getName() );
+    factory.setDataSource( dataSource() );
+    factory.setJpaProperties( jpaProperties() );
+    return factory;
+  }
+  private Properties jpaProperties() {
+    Properties properties = new Properties();
+    properties.put( "hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect" );
+    properties.put( "hibernate.show_sql", "false" );
+    return properties;
+  }
+  @Bean
+  public PlatformTransactionManager transactionManager() {
+    JpaTransactionManager txManager = new JpaTransactionManager();
+    txManager.setEntityManagerFactory( entityManagerFactory().getObject() );
+    return txManager;
+  }
 }
